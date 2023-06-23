9fde8da922b3720f8bf037c105aed5f404186c0e
shinless1412 <alejandro.troncoso1412@gmail.com>
2023-06-23 16:50:02 -0400

    1.4.1 Carrito

--- core/models.py
+++ core/models.py
@@ -3,11 +3,18 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=64)
-    categoria = models.CharField(max_length=250)
+    descripcion = models.CharField(max_length=250, verbose_name="Descripcion")
     precio = models.IntegerField()
-    imagen = models.ImageField(upload_to='productos', null=True)
+    imagen = models.ImageField(upload_to='productos/', verbose_name="imagen", null=True)
 
     def __str__(self):
-        return f'{self.nombre} -> {self.precio}'
+        return f'{self.nombre} -> {self.precio}'   
+
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
+
+
 
 # Create your models here.
+# xd
